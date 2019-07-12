0ae5469a498b8eca7a479057be035ec5748f48b7
dzjkb <jakub.pies@gmail.com>
2019-07-12 14:41:13 +0200

    add permission constants

---
+++ src/main/java/dzjkb/EasyElevator/EEPermissions.java
@@ -0,0 +1,15 @@
+package dzjkb.EasyElevator;
+
+public class EEPermissions
+{
+    public static String ADMIN = "easyelevator.admin";
+    public static String RELOAD = "easyelevator.reload";
+    public static String STOP = "easyelevator.stop";
+    public static String STOP_ALL = "easyelevator.stop.*";
+    public static String STOP_CMD = "easyelevator.stop.cmd";
+    public static String STOP_SIGN = "easyelevator.stop.sign";
+    public static String CALL = "easyelevator.call";
+    public static String CALL_ALL = "easyelevator.call.*";
+    public static String CALL_CMD = "easyelevator.call.cmd";
+    public static String CALL_SIGN = "easyelevator.call.sign";
+}
